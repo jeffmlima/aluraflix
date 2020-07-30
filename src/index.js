720101b67fb6b18393acab80655658728a47f764
Jefferson Lima <jefferson.lima.rbr@gmail.com>
2020-07-30 17:05:02 -0500

    Aula 02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+const Pagina404 = ()=>(<div>Página 404</div>)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch> {/*Ordem de prioridade, o que aparecer primeiro ele roteia, porém com exact ele é mais exigente em rotear exatamente o path*/}
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />{/*()=>(<div>Página 404</div>) Similar ao lambda do Java*/} {/* Por fim, a rota padrão, o default do switch*/}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
